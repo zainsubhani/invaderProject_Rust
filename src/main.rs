3bc2c0fed7a532c742a7556c2ed0aad2b483312a
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-09-13 16:40:26 +0200

    path

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::error::Error;
 
 use rusty_audio::Audio;
 
-fn main() -> Result<()Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
-    audio.add(git init, path);
-
+    audio.add("explode", "./src/sounds/explode.wav");
+    audio.add("lose", "./src/sounds/lose.wav");
+    audio.add("move", "./src/sounds/move.wav");
+    audio.add("pew", "./src/sounds/pew.wav");
+    audio.add("startup", "./src/sounds/startup.wav");
+    audio.add("win", "./src/sounds/win.wav");
+    Ok(())
 }
